1e0b48db3e1c6c43e39944cfbc4d5b3af6f5bb93
Anna F <anna.cx.franz29@gmail.com>
2025-06-21 01:23:30 -0400

    6/21/2025 - Sandy Bricks
    Added sandy bricks, mixed sandy bricks, mixed snady bricks style 2

---
+++ src/main/java/net/pandabear29/pandascustomblocks/block/ModBlocks.java
@@ -0,0 +1,48 @@
+package net.pandabear29.pandascustomblocks.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.pandabear29.pandascustomblocks.PandasCustomBlocks;
+import net.pandabear29.pandascustomblocks.item.ModItems;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, PandasCustomBlocks.MOD_ID);
+
+    // add blocks (copy for more blocks)
+    public static final RegistryObject<Block> SANDY_BRICKS = registerBlock("sandy_bricks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.BRICKS)));
+
+    public static final RegistryObject<Block> MIXED_SANDY_BRICKS = registerBlock("mixed_sandy_bricks",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.BRICKS)));
+
+    public static final RegistryObject<Block> MIXED_SANDY_BRICKS_TWO = registerBlock("mixed_sandy_bricks_two",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.BRICKS)));
+
+
+    // register blocks as item
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block); // register block
+        registerBlockItem(name, toReturn); // register block item
+        return toReturn;
+    }
+
+    // makes a block an item
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+
+}
---
+++ src/main/java/net/pandabear29/pandascustomblocks/item/ModCreativeModTabs.java
@@ -0,0 +1,35 @@
+package net.pandabear29.pandascustomblocks.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.pandabear29.pandascustomblocks.PandasCustomBlocks;
+import net.pandabear29.pandascustomblocks.block.ModBlocks;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PandasCustomBlocks.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> CUSTOM_BRICKS_TAB = CREATIVE_MODE_TABS.register("custom_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAND_BRICK.get()))
+                    .title(Component.translatable("creativetab.pandas_custom_blocks_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        // add items to register them to custom creative mod tab
+                        output.accept(ModItems.SAND_BRICK.get());
+
+                        output.accept(ModBlocks.SANDY_BRICKS.get());
+                        output.accept(ModBlocks.MIXED_SANDY_BRICKS.get());
+                        output.accept(ModBlocks.MIXED_SANDY_BRICKS_TWO.get());
+
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
